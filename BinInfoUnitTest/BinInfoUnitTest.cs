e72e2694bf9ed5bd0b3b1c5c9a2e40b7ee26cc9e
gustavofrizzo <guto.smo@gmail.com>
2015-07-15 22:00:22 -0300

    Adding tests.

---
+++ BinInfoUnitTest/BinInfoUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinInfo;
+
+namespace BinInfoUnitTest
+{
+    [TestClass]
+    public class BinInfoUnitTest
+    {
+        [TestMethod]
+        public void Find_BinListTest()
+        {
+            IssuerInformation info = BinList.Find("431940");
+
+            Assert.AreEqual("431940", info.Bin);
+            Assert.AreEqual("VISA", info.Brand);
+            Assert.AreEqual("IE", info.CountryCode);
+            Assert.AreEqual("Ireland", info.CountryName);
+            Assert.AreEqual("BANK OF IRELAND", info.Bank);
+            Assert.AreEqual("DEBIT", info.CardType);
+            Assert.AreEqual("53", info.Latitude);
+            Assert.AreEqual("-8", info.Longitude);
+            Assert.AreEqual("", info.SubBrand);
+            Assert.AreEqual("", info.CardCategory);
+        }
+    }
+}
