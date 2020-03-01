8ce9a1808b92e897344b35c70fc5c4f2996af0eb
Gustavo Francisco Frizzo <guto.smo@gmail.com>
2020-03-01 16:50:26 -0700

    Adding BinList.FindAsync(string bin) method.

--- BinInfo.Tests/BinListTests.cs
+++ BinInfo.Tests/BinListTests.cs
@@ -23,6 +23,22 @@ namespace BinInfo.Tests
         }
 
         [Fact]
+        public async void Find_BinListTestAsync()
+        {
+            IssuerInformation info = await BinList.FindAsync("431940");
+
+            Assert.Equal("visa", info.Scheme);
+            Assert.Equal("IE", info.Country.Alpha2);
+            Assert.Equal("Ireland", info.Country.Name);
+            Assert.Equal("BANK OF IRELAND", info.Bank.Name);
+            Assert.Equal("debit", info.CardType);
+            Assert.Equal("53", info.Country.Latitude);
+            Assert.Equal("-8", info.Country.Longitude);
+            Assert.Equal("Traditional", info.Brand);
+            Assert.False(info.Prepaid);
+        }
+
+        [Fact]
         public void ArgumentNullException_BinListTest()
         {
             Assert.Throws<ArgumentNullException>(() => BinList.Find(null));
--- BinInfo/BinList.cs
+++ BinInfo/BinList.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Threading.Tasks;
 using BinInfo.Models;
 
 namespace BinInfo
 {
     public static class BinList
     {
+        private const string BaseUrl = "https://lookup.binlist.net/";
+
         /// <summary>
         /// binlist.net is a public web service for searching Issuer Identification Numbers (IIN).
         /// <para>This webservice has an internal database with IIN/BIN information.</para>
@@ -31,12 +34,11 @@ namespace BinInfo
             {
                 try
                 {
-                    string json = web.DownloadString("https://lookup.binlist.net/" + bin);
+                    string json = web.DownloadString(BaseUrl + bin);
 
                     DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(IssuerInformation));
 
-                    var issuerInfo =
-                        (IssuerInformation)serializer.ReadObject(new MemoryStream(Encoding.UTF8.GetBytes(json)));
+                    var issuerInfo = (IssuerInformation)serializer.ReadObject(new MemoryStream(Encoding.UTF8.GetBytes(json)));
 
                     return issuerInfo;
                 }
@@ -51,5 +53,40 @@ namespace BinInfo
                 }
             }
         }
+
+#if !NET40
+        public async static Task<IssuerInformation> FindAsync(string bin)
+        {
+            if (bin == null)
+                throw new ArgumentNullException();
+
+            if (!bin.Trim().All(c => char.IsNumber(c)))
+                throw new ArgumentException("Make sure to enter a valid BIN/IIN number.");
+
+            using (WebClient web = new WebClient())
+            {
+                try
+                {
+                    string json = await web.DownloadStringTaskAsync(BaseUrl + bin);
+
+                    DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(IssuerInformation));
+
+                    var issuerInfo = (IssuerInformation)serializer.ReadObject(new MemoryStream(Encoding.UTF8.GetBytes(json)));
+                    
+                    return issuerInfo;
+                }
+                catch (WebException ex)
+                {
+                    string addInfo = $"No results for {bin}. Make sure you enter a valid BIN/IIN number. --- ";
+                    throw new WebException(addInfo + ex.Message, ex, ex.Status, ex.Response);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+#endif
+
     }
 }
