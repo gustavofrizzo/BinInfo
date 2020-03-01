a20a9e3051631c27e3486989b3ed3b903a77a433
Gustavo Francisco Frizzo <guto.smo@gmail.com>
2020-03-01 15:55:46 -0700

    Updating project to build .Net Standard 2.0.

---
+++ BinInfo.old/Models/CardNumberInformation.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace BinInfo.Models
+{
+    /// <summary>
+    /// Brief details about the card number
+    /// </summary>
+    [DataContract]
+    public class CardNumberInformation
+    {
+        /// <summary>
+        /// Length of the card number
+        /// </summary>
+        [DataMember(Name = "number")]
+        public int Length { get; set; }
+
+        /// <summary>
+        /// Whether card was tested successfully
+        /// against the Luhn algorithm 
+        /// </summary>
+        [DataMember (Name = "luhn")]
+        public bool Luhn { get; set; }
+    }
+}
